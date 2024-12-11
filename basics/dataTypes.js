eff1347d935db612a9123b7f03060c04ff97713d
SKushal123 <skushal0446@gmail.com>
2024-12-11 23:30:01 +0000

    datatypeSummary

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -9,3 +9,31 @@
 console.log(typeof null)
 
 console.log(typeof undefined)
+
+
+
+// 2 types of datatypes  primitive and reference
+// primitive 
+// 
+// -get the values from copy.. ie call by value
+// 7 types - string, number, boolean, null, undefined, symbol, bigInt
+// 
+// reference (non primitive)
+// 
+// get the values from memory
+// 
+// 3 types - array, objects, functions
+// 
+// const x=['a','b','c']
+
+// let obj={name:'x',age:'y'}
+// 
+// const x=function(){
+// console.log('hello')
+// } 
+// 
+// to know the datatype 
+// 
+// console.log(typeof )
+// 
+// learn in ECMA SCRIPT to know datatypes//
