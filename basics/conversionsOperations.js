7282afc12621f4b1f1241ef66af7713bb7787b7a
SKushal123 <skushal0446@gmail.com>
2024-12-11 14:45:27 +0000

    conversions

---
+++ basics/conversionsOperations.js
@@ -0,0 +1,25 @@
+let score='33ab';
+
+
+console.log(typeof score)
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//33=>33
+//'33abc'=>NAN not a number
+//false=>0 /true=>1
+
+
+let isLoggedIn=1
+
+isBoolean=Boolean(isLoggedIn)
+console.log(isBoolean)
+console.log(typeof isBoolean)
+
+//1=>true   0=>false  'fdffdb'=>true ' '=> false
