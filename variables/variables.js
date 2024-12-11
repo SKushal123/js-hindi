dbdc9f90e91d11b8dcc5eb46e7b6f1afa7cf9879
SKushal123 <skushal0446@gmail.com>
2024-12-11 11:51:59 +0000

    variables basics

---
+++ variables/variables.js
@@ -0,0 +1,11 @@
+const accountId=172045;
+let mailId='k@gmail.com'
+var password='12345'
+city='hyd'
+let accountState;
+
+mailId='kushal@gmail.com'
+
+console.log(accountId)
+
+console.table([accountId,mailId,password,city,accountState])
