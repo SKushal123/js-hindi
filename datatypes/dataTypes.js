60c6550fafb64243b0def5da003de65b6f63fe8c
SKushal123 <skushal0446@gmail.com>
2024-12-11 13:20:59 +0000

    datatypes

---
+++ datatypes/dataTypes.js
@@ -0,0 +1,11 @@
+//number
+//bigint
+//string
+//boolean true/false
+//undefined -typeof is undefined
+//null - stand alone value - typeof is null
+
+
+console.log(typeof null)
+
+console.log(typeof undefined)
